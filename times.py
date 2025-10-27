7f89c17d5ca09ddc5dc0b4f45504b18850126d76
xinrui su <scottsu13@outlook.com>
2025-10-27 12:09:44 +0000

    #17

--- times.py
+++ times.py
@@ -2,6 +2,9 @@ import datetime
 
 
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
+    if start_time >= end_time:
+        raise ValueError("start_time must be less than end_time")
+    
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
@@ -19,8 +22,3 @@ def compute_overlap_time(range1, range2):
             high = min(end1, end2)
             overlap_time.append((low, high))
     return overlap_time
-
-if __name__ == "__main__":
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    print(compute_overlap_time(large, short))
